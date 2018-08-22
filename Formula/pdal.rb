16aba022012e7df55112858baa00ff116a48bbd6
Matthew Stevens <mjstevens777@gmail.com>
2018-08-22 09:52:47 -0700

    Updating bottle

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -1,15 +1,16 @@
 class Pdal < Formula
   desc "Point data abstraction library"
   homepage "https://www.pdal.io/"
-  url "https://github.com/PDAL/PDAL/archive/1.7.2.tar.gz"
-  sha256 "cedfefbe54ca61cbb33d100d619c53873d84f480ff53deec2cf6dd91580f6a61"
+  # Change this back to PDAL/PDAL after 1.7.3 is released
+  url "https://github.com/mjstevens777/PDAL/archive/1.7.2.rc1.tar.gz"
+  sha256 "00d05e037a9c15a12c2bd0b51e3faec952b8a93ef20945b5ce617b2273b20ec2"
   head "https://github.com/PDAL/PDAL.git"
 
   bottle do
     root_url "https://s3.amazonaws.com/aurorasolar-envision/homebrew"
     # Do not use the bottle from the original package
     rebuild 1
-    sha256 "9d05cf0f554fead70d78c89b153cd70b424d7682ee0842770e6dfdbc1dea300d" => :high_sierra
+    sha256 "3fa1a7fdfb022d02ff5ec58901eca161314413179e72c65022aa2d7e1b095342" => :high_sierra
   end
 
   depends_on "cmake" => :build
@@ -19,6 +20,7 @@ class Pdal < Formula
   depends_on "numpy"
   depends_on "mjstevens777/auroraml/pcl"
   depends_on "postgresql"
+  depends_on "python"
 
   def install
     system "cmake", ".", *std_cmake_args,
@@ -28,7 +30,9 @@ class Pdal < Formula
                          "-DBUILD_PLUGIN_PCL=ON",
                          "-DBUILD_PLUGIN_PGPOINTCLOUD=ON",
                          "-DBUILD_PLUGIN_PYTHON=ON",
-                         "-DBUILD_PLUGIN_SQLITE=ON"
+                         "-DBUILD_PLUGIN_SQLITE=ON",
+                         # https://github.com/Kitware/CMake/blob/8901b9fe89acd4a2a739f22029e9bb473a04569f/Modules/FindPythonLibs.cmake#L29
+                         "-DPython_ADDITIONAL_VERSIONS=3.7"
 
     system "make", "install"
     doc.install "examples", "test"
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # homebrew-auroraml
 
+For easier development, work in the directory `/usr/local/Homebrew/Library/Taps/mjstevens777/homebrew-auroraml` after first pulling the tap.
+
 ## pcl
 
 To update the bottle:
+
 ```
 brew install --debug --verbose --build-bottle mjstevens777/auroraml/pcl
 brew bottle --verbose pcl
@@ -10,9 +13,11 @@ brew bottle --verbose pcl
 aws s3 cp --acl public-read pcl-1.8.1_3.high_sierra.bottle.1.tar.gz s3://aurorasolar-envision/homebrew/pcl-1.8.1_3.high_sierra.bottle.1.tar.gz
 ```
 
+## pdal
+
 ```
 brew install --debug --verbose --build-bottle mjstevens777/auroraml/pdal
 brew bottle --verbose pdal
 # Update pdal.rb with new sha256 printed at the end of the bottle command
-aws s3 cp --acl public-read pdal-VERSION.high_sierra.bottle.1.tar.gz s3://aurorasolar-envision/homebrew/pcl-VERSION.high_sierra.bottle.1.tar.gz
+aws s3 cp --acl public-read pdal*.high_sierra.bottle.tar.gz s3://aurorasolar-envision/homebrew/pdal-1.7.2.rc1.high_sierra.bottle.1.tar.gz
 ```
